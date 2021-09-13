3f8ab8d84ea5712a26c8696c1ef88f62fb9cc485
Li Zi <lonelyion@outlook.com>
2021-09-14 00:41:06 +0800

    init commit

---
+++ socket_client/MainWindow.xaml.cs
@@ -0,0 +1,237 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace socket_client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket client;
+        Task recvTask;
+        CancellationTokenSource cts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.SetOut(new ControlWriter(logTextBox));
+            sendButton.IsEnabled = false;
+        }
+        public static void LogWriteLine(string msg)
+        {
+            Console.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg));
+        }
+
+        public void NotifyClose()
+        {
+            JObject notify = new JObject();
+            notify["type"] = "disconnect";
+            client.Send(Encoding.UTF8.GetBytes(notify.ToString(Formatting.None)));
+            //已经连接
+            cts.Cancel();
+            //recvTask.Wait();
+            client.Shutdown(SocketShutdown.Both);
+            connectButton.Content = "连接服务器";
+            connectionText.Text = "未连接";
+            connectionText.Foreground = Brushes.Red;
+            sendButton.IsEnabled = false;
+        }
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sendButton.IsEnabled == false)
+            {
+                //未连接
+                LogWriteLine("开始连接");
+                connectButton.IsEnabled = false;
+                //test code
+                int port = 9047;
+                IPAddress addr;
+                IPEndPoint ep;
+                if (int.TryParse(serverPortTextBox.Text, out port) && IPAddress.TryParse(serverIPTextBox.Text, out addr))
+                {
+                    ep = new IPEndPoint(addr, port);
+                    client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+                else
+                {
+                    string msg = "服务器IP或端口填写有误，请重新填写";
+                    MessageBox.Show(msg);
+                    LogWriteLine(msg);
+                    return;
+                }
+                // 开始连接
+                try
+                {
+                    await client.ConnectAsync(ep);
+                }
+                catch (Exception ex)
+                {
+                    LogWriteLine("连接失败，可能是网络不太行");
+                    connectButton.IsEnabled = true;
+                    return;
+                }
+
+                //连接成功
+                cts = new CancellationTokenSource();
+                recvTask = Task.Run(() => Receive(cts.Token));
+
+                LogWriteLine("连接成功");
+                connectButton.Content = "断开连接";
+                connectButton.IsEnabled = true;
+            }
+            else
+            {
+                NotifyClose();
+            }
+        }
+
+        private async void Receive(CancellationToken ct)
+        {
+
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1 << 20];  //1MB
+                    int length = client.Receive(buffer);
+                    if (length == 0) continue;
+                    string message = Encoding.UTF8.GetString(buffer);
+
+
+                    JObject res = JObject.Parse(message);
+                    if (res["type"].ToString() == "ack")
+                    {
+                        //收到服务器ECHO应答，开始登录/注册
+                        LogWriteLine("已连接到服务器，正在登录");
+                        JObject o = new JObject();
+                        o["type"] = "login";
+                        o["username"] = userNameTextBox.Dispatcher.Invoke(() => userNameTextBox.Text);
+                        o["password"] = tokenTextBox.Dispatcher.Invoke(() => tokenTextBox.Text);
+                        byte[] login_buffer = Encoding.UTF8.GetBytes(o.ToString(Formatting.None));
+                        client.Send(login_buffer);
+                    }
+                    else if (res["type"].ToString() == "login")
+                    {
+                        //登录信息的返回
+                        if (res["status"].ToString() == "ok")
+                        {
+                            //登录成功
+                            LogWriteLine("登录成功，欢迎加入群聊");
+                            connectionText.Dispatcher.Invoke(() => connectionText.Text = "已连接");
+                            connectionText.Dispatcher.Invoke(() => connectionText.Foreground = Brushes.Green);
+                            sendButton.Dispatcher.Invoke(() => sendButton.IsEnabled = true);
+                            //connectionText.Text = "已连接";
+                            //connectionText.Foreground = System.Windows.Media.Brushes.Green;
+                            //sendButton.IsEnabled = true;
+                        }
+                        else if (res["status"].ToString() == "registered")
+                        {
+                            //注册成功
+                            LogWriteLine("注册成功，欢迎加入群聊");
+                            connectionText.Dispatcher.Invoke(() => connectionText.Text = "已连接");
+                            connectionText.Dispatcher.Invoke(() => connectionText.Foreground = Brushes.Green);
+                            sendButton.Dispatcher.Invoke(() => sendButton.IsEnabled = true);
+                            //connectionText.Text = "已连接";
+                            //connectionText.Foreground = System.Windows.Media.Brushes.Green;
+                            //sendButton.IsEnabled = true;
+                        }
+                        else
+                        {
+                            //登录注册失败
+                            LogWriteLine("登录失败，请检查你的用户名和口令");
+                            client.Close();
+                            return;
+                        }
+                    }
+                    else if (res["type"].ToString() == "text")
+                    {
+                        //发送消息的返回
+                        if (res["status"].ToString() != "ok")
+                        {
+                            LogWriteLine("消息发送失败");
+                        }
+                    }
+                    else if (res["type"].ToString() == "message")
+                    {
+                        //收到的其他消息
+                        if (res["status"].ToString() == "ok")
+                            LogWriteLine(res["sender"].ToString() + " : " + res["content"]);
+                    }
+                    else if (res["type"].ToString() == "shutdown")
+                    {
+                        //收到了服务器关闭的消息
+                        cts.Cancel();
+                        //recvTask.Wait();
+                        client.Shutdown(SocketShutdown.Both);
+                        connectionText.Dispatcher.Invoke(() => connectionText.Text = "未连接");
+                        connectionText.Dispatcher.Invoke(() => connectionText.Foreground = Brushes.Red);
+                        sendButton.Dispatcher.Invoke(() => sendButton.IsEnabled = false);
+                        connectButton.Dispatcher.Invoke(() => connectButton.Content = "连接服务器");
+                        LogWriteLine("服务器关闭了.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogWriteLine("Error at " + ex.Source + ": " + ex.Message);
+                    LogWriteLine(ex.StackTrace);
+                }
+            }
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string message = inputTextBox.Text;
+            JObject send = new JObject();
+            send["type"] = "text";
+            send["content"] = message;
+            byte[] buffer = Encoding.UTF8.GetBytes(send.ToString(Formatting.None));
+            if (client != null)
+            {
+                Task.Run(() => client.Send(buffer));
+            }
+        }
+
+        private void _this_Closing(object sender, CancelEventArgs e)
+        {
+            NotifyClose();
+        }
+    }
+
+    public class ControlWriter : TextWriter
+    {
+        private TextBox textbox;
+        public ControlWriter(TextBox textbox)
+        {
+            this.textbox = textbox;
+        }
+
+        public override void Write(char value)
+        {
+            textbox.Dispatcher.Invoke(new Action(() => textbox.Text += value));
+        }
+
+        public override void Write(string value)
+        {
+            textbox.Dispatcher.Invoke(new Action(() => textbox.Text += value));
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.ASCII; }
+        }
+    }
+}
---
+++ socket_server/MainWindow.xaml.cs
@@ -0,0 +1,312 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace socket_server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServerSocket instance;
+        bool isRunning = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.SetOut(new ControlWriter(logTextBox));
+        }
+
+        private void portTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            int i;
+            TextBox box = (TextBox)sender;
+            bool is_valid = int.TryParse(box.Text, out i) && i >= 1024 && i <= 65535;
+            if (!is_valid)
+            {
+                MessageBox.Show("端口应该在1024~65535之间");
+                box.Text = "9047";
+                box.Focus();
+            }
+        }
+
+        private async void controlButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isRunning)
+            {
+                int port;
+                if (int.TryParse(portTextBox.Text, out port))
+                {
+                    instance = new ServerSocket();
+                    instance.StartServer(port);
+                    statusLabel.Content = "正在运行";
+                    statusLabel.Foreground = System.Windows.Media.Brushes.Green;
+                    controlButton.Content = "关闭服务端";
+                    isRunning = true;
+                    ControlWriter.LogWriteLine("服务器开始运行");
+                }
+            }
+            else
+            {
+                //关闭服务端
+                try
+                {
+                    JObject notify = new JObject();
+                    notify["type"] = "shutdown";
+                    instance.Broadcast(notify.ToString());
+                    await Task.Delay(1000);
+                    instance.StopServer();
+                    statusLabel.Content = "未运行";
+                    statusLabel.Foreground = System.Windows.Media.Brushes.Red;
+                    controlButton.Content = "打开服务端";
+                    isRunning = false;
+                    ControlWriter.LogWriteLine("服务器结束运行");
+                }
+                catch (Exception ex)
+                {
+                    ControlWriter.LogWriteLine(ex.Message);
+                    ControlWriter.LogWriteLine(ex.StackTrace);
+                }
+            }
+        }
+    }
+
+    public class ServerSocket
+    {
+        private Socket serverSocket;
+        private bool isListening;
+        private Thread threadWatch;
+        private IPEndPoint endPoint;
+        private Dictionary<string, Socket> clientSockets;
+        private Task watchTask;
+        private CancellationTokenSource cts;
+
+        public ServerSocket() { }
+
+        public void StartServer(int port)
+        {
+            if (serverSocket == null)
+            {
+                try
+                {
+                    isListening = true;
+                    clientSockets = new Dictionary<string, Socket>();
+                    serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    endPoint = new IPEndPoint(IPAddress.Any, port);     //0.0.0.0:port
+
+                    serverSocket.Bind(endPoint);
+                    serverSocket.Listen(100);   //最大连接数
+
+                    //threadWatch = new Thread(StartListen);
+                    //threadWatch.IsBackground = true;    //后台进程
+                    //threadWatch.Start();
+                    cts = new CancellationTokenSource();
+                    watchTask = Task.Run(() => StartListen(cts.Token));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+        }
+
+        private async void StartListen(CancellationToken ct)
+        {
+            Socket? client = null;
+
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    if (serverSocket == null) return;
+                    Task t = Task.Run(() =>
+                    {
+                        try { client = serverSocket.Accept(); }     //Accept()是blocking的函数
+                        catch (SocketException) { return; }
+                    });
+                    while (await Task.WhenAny(t, Task.Delay(500)) != t)  //所以为了随时取消（
+                    {
+                        if (ct.IsCancellationRequested) return;
+                    }
+                    //client = serverSocket.Accept();
+                }
+                catch (Exception e)
+                {
+                    //TODO: Error Handling
+                }
+
+                try
+                {
+                    try { if (client == null) return; var _ = client.RemoteEndPoint; }
+                    catch (Exception e) { return; }
+                    string remoteAddr = client.RemoteEndPoint.ToString();
+                    if (!string.IsNullOrEmpty(remoteAddr))
+                    {
+                        clientSockets.Add(remoteAddr, client);  //连接成功，添加到客户端列表中
+                        ControlWriter.LogWriteLine(remoteAddr + "客户端连接成功");
+                    }
+
+                    IPAddress clientIP = (client.RemoteEndPoint as IPEndPoint).Address;
+                    int clientPort = (client.RemoteEndPoint as IPEndPoint).Port;
+                    JObject ack = new JObject();
+                    ack["type"] = "ack";
+                    ack["port"] = clientPort.ToString();
+                    byte[] sbuffer = Encoding.UTF8.GetBytes(ack.ToString());
+                    client.Send(sbuffer);
+
+                    //创建通信的线程
+                    //Thread threadRecv = new Thread(Receive)
+                    //{
+                    //    IsBackground = true
+                    //};
+                    //threadRecv.Start(client);
+
+                    Task recvTask = Task.Run(() => Receive(client));
+                }
+                catch (Exception ex)
+                {
+                    ControlWriter.LogWriteLine(ex.Message);
+                    ControlWriter.LogWriteLine(ex.StackTrace);
+                }
+            }
+        }
+
+        private void Receive(Socket client)
+        {
+            if (client == null) return;
+            while (client != null)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024 * 1024]; //1MB
+                    int len = client.Receive(buffer);
+                    if (len == 0) continue;
+                    string message = Encoding.UTF8.GetString(buffer, 0, len);
+                    ControlWriter.LogWriteLine(string.Format("客户端{0}: {1}", client.RemoteEndPoint.ToString(), message));
+
+
+                    JObject request = JObject.Parse(message);
+                    JObject response;
+                    if (request["type"].ToString() == "login")
+                    {
+                        //登录/注册请求
+                        response = new JObject();
+                        response["type"] = "login";
+                        response["status"] = new Random().Next(0, 2) == 0 ? "ok" : "registered";
+                        client.Send(Encoding.UTF8.GetBytes(response.ToString(Formatting.None)));
+                    }
+                    else if (request["type"].ToString() == "disconnect")
+                    {
+                        ControlWriter.LogWriteLine(client.RemoteEndPoint.ToString() + "断开了服务器");
+                        clientSockets.Remove(client.RemoteEndPoint.ToString());
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Dispose();
+                    } else if(request["type"].ToString() == "text")
+                    {
+                        //客户端发送了一条文本消息
+                        string content = request["content"].ToString();
+                        ControlWriter.LogWriteLine(client.RemoteEndPoint.ToString() + "发送了: " + content);
+                        response = new JObject();
+                        response["type"] = "text";
+                        response["status"] = "ok";
+                        client.Send(Encoding.UTF8.GetBytes(response.ToString(Formatting.None)));   //先返回确认收到了
+
+                        response = new JObject();
+                        response["type"] = "message";
+                        response["status"] = "ok";
+                        response["content"] = content;
+                        response["sender"] = client.RemoteEndPoint.ToString();
+                        Broadcast(response.ToString(Formatting.None)); //再广播消息
+                    }
+
+                    //if (clientSockets.Count > 0)
+                    //{
+                    //    foreach (var socket in clientSockets)
+                    //    {
+                    //        socket.Value.Send(Encoding.UTF8.GetBytes(string.Format("[{0}]{1}: {2}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), client.RemoteEndPoint.ToString(), received)));
+                    //    }
+                    //}
+
+                }
+                catch (Exception)
+                {
+                    //ControlWriter.LogWriteLine(client.RemoteEndPoint.ToString() + "断开了服务器");
+                    //clientSockets.Remove(client.RemoteEndPoint.ToString());
+
+                    //client.Close();
+                    break;
+                }
+            }
+        }
+
+        public void Broadcast(string message)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            if (serverSocket != null && clientSockets.Count > 0)
+                foreach (var socket in clientSockets)
+                {
+                    socket.Value.Send(buffer);
+                }
+        }
+
+        public void StopServer()
+        {
+            try
+            {
+                cts.Cancel();
+                watchTask.Wait();
+                foreach (var socket in clientSockets)
+                {
+                    socket.Value.Shutdown(SocketShutdown.Both);
+                }
+                clientSockets.Clear();
+                serverSocket.Close();
+                serverSocket.Dispose();
+            }
+            catch (Exception ex)
+            {
+                //Todo
+                ControlWriter.LogWriteLine(ex.Message);
+                ControlWriter.LogWriteLine(ex.StackTrace);
+            }
+        }
+    }
+    public class ControlWriter : TextWriter
+    {
+        private TextBox textbox;
+        public ControlWriter(TextBox textbox)
+        {
+            this.textbox = textbox;
+        }
+
+        public override void Write(char value)
+        {
+            textbox.Dispatcher.Invoke(new Action(() => textbox.Text += value));
+        }
+
+        public override void Write(string value)
+        {
+            textbox.Dispatcher.Invoke(new Action(() => textbox.Text += value));
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.ASCII; }
+        }
+
+        public static void LogWriteLine(string msg)
+        {
+            Console.WriteLine(string.Format("[{0}]{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), msg));
+        }
+    }
+}
+
